880fbd18dc1bd01c1a555951efeddf8c49520f05
Leon <yaroshenko.leonid@gmail.com>
2023-02-10 11:43:56 +0300

    First seminar end

---
+++ Семинар_1/Program.cs
@@ -0,0 +1,49 @@
+﻿// char c = 'c';
+// string text = "hi";
+// int i = 1;
+// bool b = true;
+// double d = 1.1;
+
+/* int num1 = 6;
+int num2 = -5;
+
+Console.WriteLine("My num is " + num1 + " ur num is " + num2);
+Console.WriteLine($"My num is {num1} ur num is {num2}"); */
+
+/*Console.Write("Enter an integer num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ur num is " + num);*/
+
+// Seminar1
+
+// Task1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Enter first num: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second num: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if (quad2 == num1)
+    Console.Write("First num is sqr of the senond num");
+else
+    Console.Write("First num is NOT sqr of the senond num");
+*/
+
+// Tsak 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Enter positive num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+Console.Write(5489 / 1000 * 1000);
