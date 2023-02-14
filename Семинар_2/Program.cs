dd99d9f8675b57739a9efb08534fbf2155775775
Leon <yaroshenko.leonid@gmail.com>
2023-02-14 10:18:45 +0300

    seminar 2 first prog

--- Семинар_2/Program.cs
+++ Семинар_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CutNumber(int num)
+{
+    int result = (num / 100) * 10 + num % 10;
+    return result;
+}
+
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine(num);
+Console.WriteLine(CutNumber(num));
